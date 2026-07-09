5d4c57c9e9138c37c3d0b5e5ddc395812982229a
imra907090-spec <imra907090@gmail.com>
2026-07-09 18:32:52 +0600

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 // config.js
 module.exports = {
-    BOT_TOKEN: "YOUR_TELEGRAM_BOT_TOKEN_HERE",
-    ADMIN_ID: 123456789, // এখানে আপনার টেলিগ্রাম আইডি দিন
+    BOT_TOKEN: "8647369071:AAEg2UdyvQGZGcxykDGkbz5oqsoabOVYOIk",
+    ADMIN_ID: 8273597769, // এখানে আপনার টেলিগ্রাম আইডি দিন
     FRONTEND_URL: "https://your-user-app-url.com", // ইউজার অ্যাপের লিঙ্ক
-    ADMIN_PANEL_URL: "https://your-admin-app-url.com" // এডমিন প্যানেলের লিঙ্ক
+    ADMIN_PANEL_URL: "content://media/external/downloads/1000008325" // এডমিন প্যানেলের লিঙ্ক
 };
