bbc609dfda67a7cfe75a858db50ea5939445e9af
imra907090-spec <imra907090@gmail.com>
2026-07-09 18:33:34 +0600

    Update config.js

--- config.js
+++ config.js
@@ -2,6 +2,6 @@
 module.exports = {
     BOT_TOKEN: "8647369071:AAEg2UdyvQGZGcxykDGkbz5oqsoabOVYOIk",
     ADMIN_ID: 8273597769, // এখানে আপনার টেলিগ্রাম আইডি দিন
-    FRONTEND_URL: "https://your-user-app-url.com", // ইউজার অ্যাপের লিঙ্ক
+    FRONTEND_URL: "content://media/external/downloads/1000008324", // ইউজার অ্যাপের লিঙ্ক
     ADMIN_PANEL_URL: "content://media/external/downloads/1000008325" // এডমিন প্যানেলের লিঙ্ক
 };
